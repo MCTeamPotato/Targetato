fa94d50c61438be155f41ebee1085eca96671c98
Kasualix <670067575@qq.com>
2023-05-12 16:30:00 +0800

    only monster

--- src/main/java/com/teampotato/targetato/mixin/MixinMobEntity.java
+++ src/main/java/com/teampotato/targetato/mixin/MixinMobEntity.java
@@ -1,6 +1,7 @@
 package com.teampotato.targetato.mixin;
 
 import com.teampotato.targetato.Targetato;
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
@@ -12,15 +13,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MobEntity.class)
-public abstract class MixinMobEntity extends LivingEntity{
+public abstract class MixinMobEntity extends LivingEntity {
     protected MixinMobEntity(EntityType<? extends LivingEntity> type, World world) {
         super(type, world);
     }
 
     @Inject(method = "setTarget", at = @At("HEAD"), cancellable = true)
     private void injectSetTarget(LivingEntity entity, CallbackInfo ci) {
-        if (this.getType().getRegistryName() == null) return;
-        if (entity instanceof PlayerEntity) return;
+        if (this.getType().getRegistryName() == null || entity instanceof PlayerEntity || !entity.getType().getCategory().equals(EntityClassification.MONSTER)) return;
         boolean checker = Targetato.LIST.get().contains(this.getType().getRegistryName().toString());
         if (Targetato.MODE.get().equals("B")) {
             if (checker) return;
