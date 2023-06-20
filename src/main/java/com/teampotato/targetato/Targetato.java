f872ba0a53f7ace8715e6e7277f985520abbb2ca
Kasualix <670067575@qq.com>
2023-06-20 18:29:34 +0800

    Advanced filter

--- src/main/java/com/teampotato/targetato/Targetato.java
+++ src/main/java/com/teampotato/targetato/Targetato.java
@@ -5,6 +5,9 @@ import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.monster.WitherSkeletonEntity;
+import net.minecraft.entity.monster.piglin.AbstractPiglinEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -17,8 +20,8 @@ import java.util.List;
 @Mod("targetato")
 public class Targetato {
     public static ForgeConfigSpec COMMON_CONFIG;
-    public static ForgeConfigSpec.ConfigValue<List<? extends String>> LIST;
-    public static ForgeConfigSpec.ConfigValue<String> MODE;
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> MOB_LIST, FILTERED_MOB_LIST, FILTERED_TARGET_LIST;
+    public static ForgeConfigSpec.ConfigValue<String> LIST_TYPE;
 
     public Targetato() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, COMMON_CONFIG);
@@ -28,27 +31,41 @@ public class Targetato {
     static {
         ForgeConfigSpec.Builder CONFIG_BUILDER = new ForgeConfigSpec.Builder();
         CONFIG_BUILDER.comment("Targetato").push("General");
-        MODE = CONFIG_BUILDER.comment("Use 'B' for blacklist, use any other word(s) (e.g.  I love you) for whitelist").define("whitelist or blacklist", "B");
-        LIST = CONFIG_BUILDER.defineList("entity list", Lists.newArrayList(), o -> o instanceof String);
+        LIST_TYPE = CONFIG_BUILDER.comment("Use 'B' for blacklist, use any other word(s) (e.g.  I love you) for whitelist").define("listType", "B");
+        MOB_LIST = CONFIG_BUILDER.defineList("mobList", Lists.newArrayList(), o -> o instanceof String);
+        CONFIG_BUILDER.pop();
+        CONFIG_BUILDER.push("Advanced Filter");
+        FILTERED_MOB_LIST = CONFIG_BUILDER.defineList("filteredMobList", Lists.newArrayList("minecraft:piglin", "minecraft:piglin_brute"), o -> o instanceof String);
+        FILTERED_TARGET_LIST = CONFIG_BUILDER.defineList("filteredTargetList", Lists.newArrayList("minecraft:wither_skeleton", "minecraft:wither_skeleton"), o -> o instanceof String);
         CONFIG_BUILDER.pop();
         COMMON_CONFIG = CONFIG_BUILDER.build();
     }
 
-    public static void injectSetTarget(LivingEntity entity, CallbackInfo ci, MobEntity mob) {
+    public static void injectSetTarget(LivingEntity target, CallbackInfo ci, MobEntity mob) {
         if (
                 mob == null ||
-                entity == null ||
+                target == null ||
                 mob.getType().getRegistryName() == null ||
-                !entity.getType().getCategory().equals(EntityClassification.MONSTER) ||
-                !(mob instanceof MonsterEntity)
+                !target.getType().getCategory().equals(EntityClassification.MONSTER) ||
+                !(mob instanceof MonsterEntity) || target.getType().getRegistryName() == null
         ) {
             return;
         }
-        boolean checker = Targetato.LIST.get().contains(mob.getType().getRegistryName().toString());
-        if (Targetato.MODE.get().equals("B")) {
+        String mobType = mob.getType().getRegistryName().toString();
+        String targetType = target.getType().getRegistryName().toString();
+        int mobIndex = FILTERED_MOB_LIST.get().indexOf(mobType);
+        int targetIndex = FILTERED_TARGET_LIST.get().indexOf(targetType);
+        if (mobIndex == targetIndex && mobIndex != -1) return;
+        boolean checker = MOB_LIST.get().contains(mobType);
+        if (LIST_TYPE.get().equals("B")) {
             if (checker) return;
         } else {
             if (!checker) return;
+        }
+        if (mob instanceof WitherSkeletonEntity && target instanceof AbstractPiglinEntity) return;
+        if (target instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) target;
+            if (player.isCreative() || player.isSpectator()) return;
         }
         ci.cancel();
     }
